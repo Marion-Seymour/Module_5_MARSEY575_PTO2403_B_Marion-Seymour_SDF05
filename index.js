953e418dec42629490bdeffb76c563da50c151e8
Marion Seymour <seymourmarion7@gmail.com>
2024-04-19 21:06:33 +0200

    Modified anda dded styling to the buttons on CSS. Modified and added the pyaer id and New Card Id on html. Added feature: reduce chips by $5 on loss in JavaScript code.

--- index.js
+++ index.js
@@ -3,24 +3,71 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard 
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-
-let isAlive = true
+let isAlive = false
 let message = ""
-
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game! 😭"
-        isAlive = false
-    }
-    console.log(message)    
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let player = {
+    name: "Per",
+    chips: 200
 }
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "Sorry, you lose this round!"
+        isAlive = false
+        player.chips -= 5; // Reduce chips by $5 when the player loses
+        playerEl.textContent = player.name + ": $" + player.chips;
+    }
+    messageEl.textContent = message
+}
+
+function newCard()  {
+
+    if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+    }
+}
