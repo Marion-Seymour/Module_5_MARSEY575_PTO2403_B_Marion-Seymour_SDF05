25f3b9f23e44124d85104735564efd1262affb15
Marion Seymour <seymourmarion7@gmail.com>
2024-04-19 16:32:58 +0200

    Added Code to js file. Added code to html file Added code to CSS file Added an image for the table background.

--- index.js
+++ index.js
@@ -3,9 +3,25 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard 
+let hasBlackJack = false
 
-let sum = firstCard + secondCard
+let isAlive = true
+let message = ""
 
-console.log(sum)
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+// 3. Log it out!
+console.log(message)
+
